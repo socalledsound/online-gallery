f7b825715d6da7b933d528234f3320e419660725
socalled kubick <socalledsound@gmail.com>
2020-11-23 12:09:21 -0800

    remove firebase config

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCcvquhO2CGo8zyIH9hGx54NN1ADeA65vg",
-    authDomain: "persistently-4625f.firebaseapp.com",
-    databaseURL: "https://persistently-4625f.firebaseio.com",
-    projectId: "persistently-4625f",
-    storageBucket: "persistently-4625f.appspot.com",
-    messagingSenderId: "526323833103",
-    appId: "1:526323833103:web:93182b3faf5f09c571867a"
+    // put your firebase config here, available from your firebase console:
+    //https://console.firebase.google.com/ 
+    //go to you project then settings and scroll down
     };
+
+
+
+    const drop = document.querySelector('#drop');
+
+
+    window.addEventListener('drag', (e)=>{
+        e.preventDefault();
+    })
+
+    drop.addEventListener('drop', (e)=>{
+        e.preventDefault();
+        const file = e.dataTransfer.files[0];
+        const reader = new FileReader();
+        const fileSize = file.size/ 1024 / 1024;
+        if(file){
+            if(fileSize > 1){
+                alert('image needs to be smaller than 1MB');
+            }
+
+            reader.readAsDataURL(file);
+        } 
+
+        reader.addEventListener('load', (e) =>{
+            const fileToUpload = reader.result;
+            const metadata = 
+        })
+
+
+        }
+    })
+
